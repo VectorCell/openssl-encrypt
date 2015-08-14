ea5cec65ef18131eaf02c3fd3f9adbe6d3a95466
Brandon Smith <vectorcell@gmail.com>
2015-08-14 06:35:20 -0500

    added symmetric aes scripts

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 destdir="$HOME/bin"
 
-for file in $(ls | grep rsa); do
+for file in $(ls | grep -E "rsa|aes"); do
 	echo "$file -> $destdir/$file"
 	ln -s "$DIR/$file" "$destdir/$file"
 done
